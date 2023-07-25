72997ffc1158d2a5f667a179f6bcefde4d29b49b
Jitendra Tiwari <jitendra.tiwari@redaptiveinc.com>
2023-07-25 13:29:46 +0530

    Cors config

--- src/main/java/com/jitendra/myportfolio/config/SecurityConfiguration.java
+++ src/main/java/com/jitendra/myportfolio/config/SecurityConfiguration.java
@@ -1,18 +1,28 @@
 package com.jitendra.myportfolio.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain customSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests((requests) -> requests.requestMatchers(HttpMethod.POST, "/articles")
+
+         return httpSecurity.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(request -> {
+             CorsConfiguration configuration = new CorsConfiguration();
+             String allowedOrigin = System.getenv("ALLOWED_ORIGINS");
+             configuration.setAllowedOrigins(Arrays.asList(allowedOrigin.split(",")));
+             return configuration;
+         }))
+             .authorizeHttpRequests((requests) -> requests.requestMatchers(HttpMethod.POST, "/articles")
                                                                         .authenticated()
                                                                         .requestMatchers(HttpMethod.GET, "/articles")
                                                                         .permitAll()
